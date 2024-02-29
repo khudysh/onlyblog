5476a8fc10c3fb40c9ebb56325b91453387c367b
khudysh <ehd2001@mail.ru>
2024-02-29 13:09:55 +0300

    Profile page styles

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "../../hooks/hooksState";
-import { Card, Avatar, Descriptions, DescriptionsProps, Button } from "antd";
+import { Card, Avatar, Descriptions, DescriptionsProps, Button, Badge } from "antd";
 import "./Profile.scss"
 import { userType } from "../../store/user/userTypes";
 import { getUserProfile } from "../../store/blog/blogActions";
@@ -56,6 +56,19 @@ function Profile() {
         <Descriptions title="User Info" items={items} />
         <Link to={`/logout`}><Button>Logout</Button></Link>
       </Card>
+
+      <div className="profile-posts">
+        {posts.map((post) => {
+          return <Card className="profile-posts-post" key={post.id} title={post.title} extra={<a href="#">More</a>} >
+            <p>{post.body}</p>
+            <hr />
+            {post.tags.map((tag) => {
+              return <Badge key={tag} count={tag} color='#faad14' />
+            })}
+            <p>👍 {post.reactions}</p>
+          </Card>
+        })}
+      </div>
     </div>
   )
 }
